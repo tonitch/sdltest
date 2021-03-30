878bf241b01af0107fd204bd575c45dced6872be
Anthony Debucquoy <d.tonitch@gmail.com>
2021-03-30 03:13:57 +0200

    Correcting memory leak

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include "GameWindow.hpp"
+#include <iostream>
+#include <math.h>
+
+bool drawFirstRun = true;
+SDL_Texture* img;
+double i = 0;
 
 bool event(SDL_Window* win, SDL_Renderer* ren){
 	return 0;
 }
 
 bool draw(SDL_Window* win, SDL_Renderer* ren){
-	SDL_Texture* img = IMG_LoadTexture(ren, "tonitch.png");
+	if(drawFirstRun){
+		drawFirstRun = false;
+		img = IMG_LoadTexture(ren, "tonitch.png");
+	}
+	i = i + 0.05;
 	SDL_Rect dstrect;
-	dstrect.x = 0;
-	dstrect.y = 0;
+	dstrect.x = sin(i) * 200 + 400;
+	dstrect.y = cos(i) * 200 + 400;
 	dstrect.w = 50;
 	dstrect.h = 50;
 	SDL_RenderCopy(ren, img, NULL, &dstrect);
